1b2a2bc3d9d2fdc10709868bb5e6454b02695bfd
Seasonal Matcha <31607849+seasonalmatcha@users.noreply.github.com>
2020-09-22 09:18:08 +0700

    Update index.js
    Coba dicek comment nya. Perbaiki lagi

--- index.js
+++ index.js
@@ -4,28 +4,47 @@ const client = new Discord.Client();
 const kategori = "ID-CATEGORY";
 const voiceutama = "ID-VOICE";
 
+// Ini cukup sekali aja di awal. Gausah tiap user join.
+client.userVoice = new Discord.Collection();
+
 client.on('ready', () => {
   console.log("bot has online");
 });
 
-client.on('message', message => {
-	console.log(message.content);
-});
+// Message event gaperlu.
 
 client.on('voiceStateUpdate', (oldState, newState) => {
   const oldID = oldState.channel ? oldState.channel.id : undefined;
   const newID = newState.channel ? newState.channel.id : undefined;
 
   if (newID === voiceutama) {
+    const voiceData = client.userVoice.get(newState.id);
 
-    client.userVoice = new Discord.Collection()
-    const voiceData = client.userVoice.get(newState.member.user.id);
-  
-    if (client.userVoice.has(newState.member.user.id)) {
+    if (voiceData) {
       const voiceChannel = newState.guild.channels.cache.get(voiceData.voiceID);
   
       return newState.setChannel(voiceChannel);
     } else
+      /* 
+        Di sini cek dulu member punya role investor/senior atau ga.
+	      Kalo punya, pake permissions yang ini:
+          {
+            id: newState.guild.id,
+            allow: ['CONNECT', 'SPEAK'],
+          },
+          {
+            id: newState.member.user.id,
+            allow: ['CONNECT', 'SPEAK', 'MANAGE_CHANNELS', ],
+          }
+	  
+	      Kalo gapunya, pake ini
+	        {
+            id: newState.guild.id,
+            allow: ['CONNECT', 'SPEAK'],
+          },
+	  
+	      Jangan semuanya dibikin permissions yang sama.
+      */
       return newState.guild.channels.create(`${newState.member.user.username} Channel`, {
         type: "voice",
         parent: kategori,
@@ -52,12 +71,12 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   }
 
   if (oldID) {
-    if (client.userVoice.find(x => x.voiceID.includes(oldID) !== undefined)) {
-      const voiceData = client.userVoice.find(x => x.voiceID.includes(oldID));
-  
+    const voiceData = client.userVoice.find(x => x.voiceID === oldID);
+
+    if (voiceData) {  
       const voiceChannel = oldState.guild.channels.cache.get(voiceData.voiceID);
   
-      if (!voiceChannel.members.size) voiceChannel.delete();
+      if (voiceChannel.members.filter(x => !x.user.bot).size < 1) voiceChannel.delete();
   
       return client.userVoice.delete(voiceData.ownerID);
     }
